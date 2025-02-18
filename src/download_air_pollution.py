a074a15bc080e34c8031ce66de045bedc8e21d8b
audiracmichelle <audiracmichelle@gmail.com>
2025-02-18 01:51:47 -0500

    modified header

--- src/download_air_pollution.py
+++ src/download_air_pollution.py
@@ -8,8 +8,13 @@ from tqdm import tqdm
 def main(cfg):
     url = f"{cfg.url}{cfg.filename}"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Connection": "keep-alive"
     }
+
 
     session = requests.Session()
     retry = Retry(
