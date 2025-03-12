61a097c4a36efe25903707c3aaa7baf3c6bc6c37
audiracmichelle <audiracmichelle@gmail.com>
2025-03-12 11:04:27 -0400

    created sbatch jobs

---
+++ jobs/jobs.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sbatch jobs/pm25_2000.sbatch
+sbatch jobs/pm25_2001.sbatch
+sbatch jobs/pm25_2002.sbatch
+sbatch jobs/pm252003.sbatch
+sbatch jobs/pm25_2004.sbatch
+sbatch jobs/pm25_2005.sbatch
+sbatch jobs/pm25_2006.sbatch
+sbatch jobs/pm25_2007.sbatch
+sbatch jobs/pm25_2008.sbatch
+sbatch jobs/pm25_2009.sbatch
+sbatch jobs/pm25_2010.sbatch
+sbatch jobs/pm25_2011.sbatch
+sbatch jobs/pm25_2012.sbatch
+sbatch jobs/pm25_2013.sbatch
+sbatch jobs/pm25_2014.sbatch
+sbatch jobs/pm25_2015.sbatch
+sbatch jobs/pm25_2016.sbatch
